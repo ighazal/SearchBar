ac01e51bf30e4e216688721dbe025fd6377bb7f4
Ghazal <taranehjj489@gmail.com>
2023-07-25 17:31:47 +0430

    done

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <input id="inp" type="text" v-model="text" @keyup="information" placeholder="Type your keyword">
+    <input type="text" v-model="text" @keyup="information" placeholder="Type your keyword">
     <div v-for="post in posts" :key="post.Title">
-      <p> {{ post.Type }} </p>
-      <!--<post_a :post="post"></post_a>-->
+        <p class="res"> {{ post.Title }} , {{ post.Year }} </p>
     </div>
   </div>
 </template>
 
 <script>
-/*import post_a from './components/Post';*/
 import axios from 'axios';
 export default{
   name: "app",
-  /*components: {post_a},*/
   setup() {
     return{
     }
@@ -21,7 +18,7 @@ export default{
   data() {
     return {
       text: '',
-      posts : null
+      posts : null,
     }
   },
   methods:{
@@ -30,13 +27,12 @@ export default{
 };
 function information() {
     let searchQuery = this.text;
-    console.log("here");
-    console.log(searchQuery);
     const url = `https://www.omdbapi.com/?s=${searchQuery}&page=2&apikey=4c1da51e`
     axios
       .get(url)
       .then((response) => {
-        this.posts = response
+        this.posts = response.data.Search;
+        response.data.Search = response;
       })
       .catch((error) => {
         this.posts  = error
@@ -49,5 +45,14 @@ function information() {
   text-align: center;
   color: lightslategrey;
   margin-top: 60px;
+}
+.res{
+  width: 500px;
+  height: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  color: black;
+  font-size: 20px;
+  background-color: rgb(238, 144, 218);
 }
 </style>
